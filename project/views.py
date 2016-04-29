49db7f46220dc53943d124ba351485f6758ece4d
Hao Dinh <h.c.dinh1@gmail.com>
2016-04-29 07:47:24 +0200

    managing sessions

--- project/views.py
+++ project/views.py
@@ -19,7 +19,7 @@ from models import Task
 from models import User
 
 
-#helper functions
+#######helper functions######
 def login_required(test):
 	@wraps(test)
 	def wrap(*args, **kwargs):
@@ -34,6 +34,7 @@ def login_required(test):
 @app.route('/logout/')
 def logout():
 	session.pop('logged_in', None)
+	session.pop('user_id', None)
 	flash('Goodbye!')
 	return redirect(url_for('login'))
 
@@ -46,6 +47,7 @@ def login():
 			user = User.query.filter_by(name=request.form['name']).first()
 			if user is not None and user.password == request.form['password']:
 				session['logged_in'] = True
+				session['user_id'] = user.id
 				flash('Welcome!')
 				return redirect(url_for('tasks'))
 			else:
@@ -67,7 +69,7 @@ def tasks():
 	)
 
 #Add new tasks
-@app.route('/add/', methods=['POST'])
+@app.route('/add/', methods=['POST', 'GET'])
 @login_required
 def new_task():
 	form = AddTaskForm(request.form)
@@ -79,12 +81,16 @@ def new_task():
 				form.priority.data,
 				datetime.datetime.utcnow(),
 				'1',
-				'1'
+				session['user_id']
 				)
 			db.session.add(new_task)
 			db.session.commit()
 			flash('New entry was successfully posted. Thanks.')
-	return redirect(url_for('tasks'))
+			return redirect(url_for('tasks'))
+		else:
+			flash('All fields are required.')
+			return redirect(url_for('tasks'))
+	return render_template('tasks.html', form=form)
 
 
 #Mark tasks as complete
